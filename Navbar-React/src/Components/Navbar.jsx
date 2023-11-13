beeb48e51d0f18e79c71f4dcd0a46c52c44b666b
Nicolas Sebastian Capriz <nscapriz@do.scba.gov.ar>
2023-11-13 14:00:17 -0300

    Primer commit

---
+++ Navbar-React/src/Components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+import logo from "../assets/img/barra-navegación.png"
+import Menu from "../assets/img/menu-hamburguesa.png"
+
+const Navbar = () => {
+    const [menu, setMenu] = useState(false);
+  
+    const toggleMenu = () => {
+      setMenu(!menu);
+    };
+  
+    return (
+      <div className="container_navbar">
+        <div className="container_navbar--logo">
+            <img src={ logo } alt="Logo" />
+            <p>NavBar</p>
+        </div>
+        <div className="container_navbar--links">
+            <button ><img src={ Menu } alt="menu" /></button>
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+            </ul>
+        </div>
+      </div>
+    );
+  };
+
+export default Navbar
+
